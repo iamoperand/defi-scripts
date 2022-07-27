143b80882959aee9cad1f255aef3881951bab145
nikhil arora <narora200@gmail.com>
2022-07-27 12:28:09 +0530

    feat: deposit to aave v2

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,27 +6,23 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 
 module.exports = {
-    solidity: "0.8.7",
+    solidity: {
+        compilers: [{ version: "0.8.7" }, { version: "0.6.12" }, { version: "0.4.19" }],
+    },
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
             chainId: 31337,
             blockConfirmations: 1,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
         },
         localhost: {
             chainId: 31337,
-        },
-        rinkeby: {
-            chainId: 4,
-            blockConfirmations: 6,
-            url: RINKEBY_RPC_URL,
-            accounts: [PRIVATE_KEY],
         },
     },
     namedAccounts: {
@@ -45,12 +41,8 @@ module.exports = {
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
-        coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
     mocha: {
         timeout: 300000, // 300 seconds max
-    },
-    etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
     },
 }
